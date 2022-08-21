975fd79686741f738754028caae57422673efba2
Barbara Gichohi <my.github.barbaragichohi77@gmail.com>
2022-08-21 21:02:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+    
+}
+
+
+ function setBestCustomer(undefined) {
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'value'
+ function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'value2'
+ }
